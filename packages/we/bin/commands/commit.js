b72039031e5ae0c736edf1ed798a53d0a24fc326
Bill Fisk <contact@bulldogandfisk.com>
2022-01-07 15:35:31 -0500

    feat: dummy

--- packages/we/bin/commands/commit.js
+++ packages/we/bin/commands/commit.js
@@ -1,5 +1,6 @@
 import shell from 'shelljs';
 
+// comment!
 export default (message = 'no commit message') => {
     if(shell.exec(`git add . && git commit -m "${message}"`).code !== 0) {
         shell.echo('Error: Git commit failed');
